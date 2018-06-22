79a95b91d4db726838e9e0ec98c023b4a8b2cb76
Adrián Garza <agarzas@outlook.com>
2018-06-22 07:08:22 -0600

    pagination

--- src/components/pagination.template.ts
+++ src/components/pagination.template.ts
@@ -1,7 +1,6 @@
 export const PAGINATION_TEMPLATE = `
 <div class="pagination-box">
     <div class="pagination-range">
-        {{dataTable.translations.paginationRange}}:
         <span [textContent]="dataTable.offset + 1"></span>
         -
         <span [textContent]="[dataTable.offset + dataTable.limit , dataTable.itemCount] | min"></span>
@@ -9,14 +8,6 @@ export const PAGINATION_TEMPLATE = `
         <span [textContent]="dataTable.itemCount"></span>
     </div>
     <div class="pagination-controllers">
-        <div class="pagination-limit">
-            <div class="input-group">
-                <span class="input-group-addon">{{dataTable.translations.paginationLimit}}:</span>
-                <input #limitInput type="number" class="form-control" min="1" step="1"
-                       [ngModel]="limit" (blur)="limit = limitInput.value"
-                       (keyup.enter)="limit = limitInput.value" (keyup.esc)="limitInput.value = limit"/>
-            </div>
-        </div>
         <div class=" pagination-pages">
             <button [disabled]="dataTable.offset <= 0" (click)="pageFirst()" class="btn btn-default pagination-firstpage">&laquo;</button>
             <button [disabled]="dataTable.offset <= 0" (click)="pageBack()" class="btn btn-default pagination-prevpage">&lsaquo;</button>
@@ -26,8 +17,7 @@ export const PAGINATION_TEMPLATE = `
                            [ngModel]="page" (blur)="page = pageInput.value"
                            (keyup.enter)="page = pageInput.value" (keyup.esc)="pageInput.value = page"/>
                     <div class="input-group-addon">
-                        <span>/</span>
-                        <span [textContent]="dataTable.lastPage"></span>
+                        <span [textContent]="'/ '+dataTable.lastPage"></span>
                     </div>
                 </div>
             </div>
